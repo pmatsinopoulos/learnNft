6a22f21a26385de5f9c1ad1b2c178e52ee9a15cb
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-07-12 22:59:42 +0200

    Use public state variables to avoid having getter methods

--- contracts/OrangeToken.sol
+++ contracts/OrangeToken.sol
@@ -2,19 +2,11 @@
 pragma solidity 0.8.24;
 
 contract OrangeToken {
-    string private _name;
-    string private _symbol;
+    string public name;
+    string public symbol;
 
     constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
+        name = name_;
+        symbol = symbol_;
     }
 }
